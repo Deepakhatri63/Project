654ab4c3047de7b7fb4f47c7dc2ba7b5915a5b2f
Deepak <deepakkhatri17d@gmail.com>
2023-11-25 16:07:27 +0530

    resposive

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var infoTitle = document.querySelector(".info-title");
+    var infoPara = document.querySelector(".info-para");
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function handleScroll() {
+        if (isInViewport(infoTitle)) {
+            infoTitle.classList.add("animated");
+        }
+        if (isInViewport(infoPara)) {
+            infoPara.classList.add("animated");
+        }
+    }
+
+    // Initial check in case the elements are already in the viewport on page load
+    handleScroll();
+
+    // Listen for scroll events
+    window.addEventListener("scroll", handleScroll);
+});
