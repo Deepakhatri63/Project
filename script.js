df72be4065a388076db9bf68a5520326c8fb22d4
Deepak <deepakkhatri17d@gmail.com>
2023-11-26 15:08:32 +0530

    responsive

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var infoTitle = document.querySelector(".info-title");
-    var infoPara = document.querySelector(".info-para");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var infoTitle = document.querySelector(".info-title");
+//     var infoPara = document.querySelector(".info-para");
+//     // var square = document.querySelector(".square");
 
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+//     function isInViewport(element) {
+//         var rect = element.getBoundingClientRect();
+//         return (
+//             rect.top >= 0 &&
+//             rect.left >= 0 &&
+//             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//         );
+//     }
 
-    function handleScroll() {
-        if (isInViewport(infoTitle)) {
-            infoTitle.classList.add("animated");
-        }
-        if (isInViewport(infoPara)) {
-            infoPara.classList.add("animated");
-        }
-    }
+//     function handleScroll() {
+//         if (isInViewport(infoTitle)) {
+//             infoTitle.classList.add("animated");
+//         }
+//         if (isInViewport(infoPara)) {
+//             infoPara.classList.add("animated");
+//         }
+//         // if (isInViewport(square)) {
+//         //     infoPara.classList.add("animated");
+//         // }
+//     }
 
-    // Initial check in case the elements are already in the viewport on page load
-    handleScroll();
+//     // Initial check in case the elements are already in the viewport on page load
+//     handleScroll();
 
-    // Listen for scroll events
-    window.addEventListener("scroll", handleScroll);
-});
+//     // Listen for scroll events
+//     window.addEventListener("scroll", handleScroll);
+// });
